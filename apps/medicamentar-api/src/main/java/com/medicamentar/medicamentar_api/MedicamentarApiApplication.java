6292d90b4d3dc61636eaabfd082173d3945724c5
Bruno0M <brunomedeirosr09@gmail.com>
2024-08-09 22:24:11 -0300

    feat: troca o back-end pra java 😭😭

---
+++ apps/medicamentar-api/src/main/java/com/medicamentar/medicamentar_api/MedicamentarApiApplication.java
@@ -0,0 +1,13 @@
+package com.medicamentar.medicamentar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicamentarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicamentarApiApplication.class, args);
+	}
+
+}
---
+++ apps/medicamentar-api/src/test/java/com/medicamentar/medicamentar_api/MedicamentarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicamentar.medicamentar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicamentarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
