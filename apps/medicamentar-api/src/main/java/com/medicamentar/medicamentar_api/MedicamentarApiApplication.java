77f5c2e9096c7cbfb91d32dac29b1fe5f6461997
Bruno0M <brunomedeirosr09@gmail.com>
2024-08-10 19:03:07 -0300

    feat: adiciona o swagger ao projeto - adiciona uma rota 'HelloWorld' como template

--- apps/medicamentar-api/src/main/java/com/medicamentar/medicamentar_api/MedicamentarApiApplication.java
+++ apps/medicamentar-api/src/main/java/com/medicamentar/medicamentar_api/MedicamentarApiApplication.java
@@ -3,7 +3,11 @@ package com.medicamentar.medicamentar_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "Medicamentar API"))
 public class MedicamentarApiApplication {
 
 	public static void main(String[] args) {
---
+++ apps/medicamentar-api/src/main/java/com/medicamentar/medicamentar_api/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.medicamentar.medicamentar_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping(value = "/api/v1/HelloWorld", produces = {"application/json"})
+@Tag(name = "HelloWorld!")
+public class HelloWorldController {
+  @Operation(summary = "Pequena descrição do endpoint", method = "GET")
+  @GetMapping()
+  public String helloWorld() {
+      return "Hello World!";
+  }
+}
