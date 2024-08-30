78640879812e4b7fdac1141a2c27ff7c629c50ab
TiagosailE <tiagosantos.eli@gmail.com>
2024-08-30 14:13:35 -0300

    feat: adiciona o crud de exames

---
+++ apps/medicamentar-api/src/main/java/com/medicamentar/medicamentar_api/api/controller/ExamController.java
@@ -0,0 +1,49 @@
+package com.medicamentar.medicamentar_api.api.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medicamentar.medicamentar_api.application.dtos.ExamDto.ExamRequest;
+import com.medicamentar.medicamentar_api.application.services.ExamService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/exam")
+@Tag(name = "Exam")
+@RequiredArgsConstructor
+public class ExamController {
+
+    private final  ExamService service;
+
+    @GetMapping
+    public ResponseEntity getAllexams(){
+        var allExams = this.service.getAllexams();
+        return ResponseEntity.ok(allExams);
+    }
+
+    @PostMapping
+    public  ResponseEntity registerExam(@RequestBody @Valid ExamRequest data){
+        this.service.registerExam(data);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExam(@PathVariable UUID id){
+        var response = this.service.deleteExam(id);
+        return ResponseEntity.ok(response);
+    }
+
+}
+    
+    
---
+++ apps/medicamentar-api/src/main/java/com/medicamentar/medicamentar_api/application/DTOs/ExamDto/ExamRequest.java
@@ -0,0 +1,18 @@
+package com.medicamentar.medicamentar_api.application.dtos.ExamDto;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ExamRequest(
+    @NotNull(message = "Date can't be null")
+    Date date,
+    @NotBlank(message = "Name can't be blank")
+    String name, 
+    @NotBlank(message= "local can't be blank")
+    String local,
+    
+    String description) {
+    
+}
