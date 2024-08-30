0602bbcbd707d59086a363e2586726a5d9ce19a8
Lushlitz <pedhcavdsan1603@gmail.com>
2024-08-29 22:56:18 -0300

    fix: correção do Header e sua implementação

--- apps/medicamentar-desktop/src/components/Header.tsx
+++ apps/medicamentar-desktop/src/components/Header.tsx
@@ -1,35 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, AppBar } from "@mui/material";
 import logoWhite from "../../public/assets/icons/logoWhite.svg";
 import logoBlue from "../../public/assets/icons/logoBlue.svg";
 import { useLocation } from "react-router-dom";
 import DarkModeToggle from "./ThemeSwitcherButton";
 
-export default function Header() {
+function Header() {
   const location = useLocation().pathname;
-  const condition = location == "/register" || location == "/register";
+  const condition = location == "/signin" || location == "/register";
   return (
-    <Box
-    sx={{
-      width: "100vw",
-      height: "100px",
-      backgroundColor:
-        condition ? "transparent" : theme => theme.palette.primary.light,
-      padding: "27px 45px",
-      display: "flex",
-    }}
-  >
-    <Box sx={{ 
-      width: "100vw", 
-      display: "flex", 
-      justifyContent: condition ? "space-between" : "space-between" 
-      }}>
-      <img
-        style={{ width: "255px", height: "46px", margin: condition ? "0" : "0 auto" }}
-        src={ condition ? logoBlue : logoWhite}
-        alt="medicamentar logo"
-      />
-      <DarkModeToggle/>
-    </Box>
-  </Box>
+    <AppBar
+      position="fixed"
+      sx={{
+        width: "100vw",
+        height: "100px",
+        backgroundColor: condition
+          ? "transparent"
+          : (theme) => theme.palette.primary.light,
+        padding: "27px 45px",
+        justifyContent: "space-between",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        zIndex: 1201,
+        boxShadow: condition ? 0 : 3
+      }}
+    >
+
+      <Box sx={{ flex: condition ? 1 : 0 }} />
+      <Box sx={{ flex: 1, display: "flex", justifyContent: condition ? "center" : "normal", alignItems: "center" }}>
+        <img
+          style={{ width: "255px", height: "46px" }}
+          src={condition ? logoBlue : logoWhite}
+          alt="medicamentar logo"
+        />
+      </Box>
+      <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
+        <DarkModeToggle />
+      </Box>
+    </AppBar>
   );
 }
+export default Header;
