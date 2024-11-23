c2ddbf7ff827864f556c7b416e2bda03912d65ea
PHCavalcante <pedhcavdsan1603@gmail.com>
2024-11-23 14:10:10 -0300

    refactor: refatorando Types para novas respostas do back-end

--- apps/medicamentar-desktop/src/types/gridItemTypes.ts
+++ apps/medicamentar-desktop/src/types/gridItemTypes.ts
@@ -1,10 +1,11 @@
 import { actionTypes } from "./actionTypes";
 
 export type gridItemTypes = {
+    id: string,
     description: string,
     name?: string,
+    doctorName?: string,
     actionType: actionTypes,
     date: string,
     eventDate?: string,
-    medic?: string
 };
