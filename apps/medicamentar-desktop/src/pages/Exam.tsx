f11f2345364e0504b2c63477c158380e9fb9679d
Pyments <eduardo.spmt@gmail.com>
2024-11-24 14:41:12 -0300

    feat: implementa paginação nas páginas de eventos, exames e medicamentos

--- apps/medicamentar-desktop/src/pages/Exam.tsx
+++ apps/medicamentar-desktop/src/pages/Exam.tsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from "react";
 import Header from "@components/Header";
 import SideBar from "@components/SideBar";
+import { useEffect, useState } from "react";
 import { AddBtn } from "@components/AddBtn";
+import axiosInstance from "@utils/axiosInstance";
 import ExamModal from "@components/Modals/ExamModal";
 import CardUniversal from "@components/CardUniversal";
-import { Box, Grid, Typography, Stack } from "@mui/material";
 import { SectionContainer } from "@components/SectionContainer";
 import { ContainerUniversal } from "@components/ContainerUniversal";
-import axiosInstance from "@utils/axiosInstance";
+import { Box, Grid, Typography, Stack, Pagination } from "@mui/material";
+
 import { useTheme } from "@constants/theme/useTheme";
 import { useLocalStorage } from "@hooks/UseLocalStorage";
 
-interface ExamData{
-  id:string,
-  type:string,
-  name: string,
-  local:string,
-  date:string,
+interface ExamData {
+  id: string;
+  type: string;
+  name: string;
+  date: string;
+  local: string;
 }
 interface User {
   token: {
@@ -29,31 +30,39 @@ const Exam = () => {
   const [user] = useLocalStorage<User | null>("user", null);
   const token = user?.token.data;
   const [exams, setExams] = useState<ExamData[]>([]);
+  const [page, setPage] = useState(0);
+  const [pageCount, setPageCount] = useState<number>(0);
 
+  const fetchExams = async () => {
+    try {
+      const response = await axiosInstance.get(`/exam?page=${page}&size=9`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("Lista de Exames e Consultas:", response.data.data);
+      setExams(response.data.data);
+      setPageCount(response.data.getTotalPages);
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  };
 
-  
-    const fetchExams  = async () => {
-      try {
-        const response = await axiosInstance.get("/exam", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        console.log("Lista de Exames e Consultas:",response.data.data);
-        setExams(response.data.data);
-      } catch (error) {
-        console.error("Erro na requisição:", error);
-      }
-    };
-
-    useEffect(() => {
-      if (token) {
-        fetchExams();
-      }
-    }, [token]);
+  useEffect(() => {
+    if (token) {
+      fetchExams();
+    }
+  }, [token, page]);
 
   const handleModal = () => {
     setOpen(!open);
+  };
+
+  const handlePagination = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value - 1);
   };
 
   return (
@@ -87,7 +96,7 @@ const Exam = () => {
           <AddBtn handleModal={handleModal} text="consulta ou exame" />
         </Stack>
         <Grid container spacing={3} pb="75px">
-        {exams.length > 0 ? (
+          {exams.length > 0 ? (
             exams.map((exam) => (
               <CardUniversal
                 key={exam.id}
@@ -108,10 +117,36 @@ const Exam = () => {
               Nenhuma consulta ou exame encontrado.
             </Typography>
           )}
-         
+          {pageCount > 1 && (
+            <Grid
+              item
+              xs={12}
+              sx={{ display: "flex", justifyContent: "center", mt: 2 }}
+            >
+              <Pagination
+                page={page + 1}
+                color="primary"
+                count={pageCount}
+                onChange={handlePagination}
+                sx={{
+                  "& .MuiPaginationItem-ellipsis": {
+                    color: darkMode ? "common.white" : "primary.main",
+                  },
+                  "& .MuiPaginationItem-page.Mui-selected": {
+                    backgroundColor: darkMode
+                      ? "primary.darker"
+                      : "primary.main",
+                    color: "white",
+                  },
+                }}
+              />
+            </Grid>
+          )}
         </Grid>
       </SectionContainer>
-      {open && <ExamModal open={open} onClose={handleModal} fetchExams={fetchExams} />}
+      {open && (
+        <ExamModal open={open} onClose={handleModal} fetchExams={fetchExams} />
+      )}
     </ContainerUniversal>
   );
 };
